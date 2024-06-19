493d8882c10adb191abbc1a3ef8e40c21e392b88
Night1ng4le <2276811587@qq.com>
2024-06-19 23:27:04 +0800

    lab00 test

--- lab00/lab00.py
+++ lab00/lab00.py
@@ -1,9 +1,8 @@
-def twenty_twenty_three():
-    """Come up with the most creative expression that evaluates to 2023,
+def twenty_twenty_four():
+    """Come up with the most creative expression that evaluates to 2024
     using only numbers and the +, *, and - operators.
 
-    >>> twenty_twenty_three()
-    2023
+    >>> twenty_twenty_four()
+    2024
     """
-    return 1+1011*2
-
+    return 2024
